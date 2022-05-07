297d643f161d4bfcd31e9055f81a8f48aaf0fae9
iblis <kannafay@qq.com>
2022-05-08 02:34:05 +0800

    reset

--- inc/frame-js.php
+++
@@ -1,7 +0,0 @@
-<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/backtop.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/vue.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/config.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/theme.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/custom/iconfont/iconfont.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/custom/user.js"></script>
--- inc/searchform-mobile.php
+++
@@ -1,6 +0,0 @@
-<form role="searchform-mb" method="get" id="searchform-mb" class="searchform-mb" action="<?php bloginfo('url') ?>">
-	<div>
-		<input type="text-" value="" name="s" id="s" placeholder="搜索这个世界">
-        <button type="submit"><span class="iconfont icon-sousuo"></span></button>
-	</div>
-</form>
